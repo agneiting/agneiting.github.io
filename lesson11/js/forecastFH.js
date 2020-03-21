0fbcbbe49ec9334279dcc169c6335daa34e5efc9
agneiting <amber.wernli50@gmail.com>
2020-03-21 12:55:52 -0600

    commit

---
+++ lesson11/js/forecastFH.js
@@ -0,0 +1,32 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5585010&units=imperial&appid=04076ec099a473169d26a85db37740a3';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    //Create day of week array to later show the day of the week in the table.
+    const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+    //Only look where the time is 18:00:00.
+    const fiveDayForecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+    console.log(fiveDayForecast);
+
+    //Increment through the 5 days to add info.
+    for (let i=0; i<fiveDayForecast.length; i++) {
+        //Set day.
+        currentDate = new Date()
+        cDay = currentDate.getDay()
+        nextDay = dayOfWeek[cday+1]
+        document.getElementById(`day${i+1}`).textContent = nextDay;
+        
+        //Set temperature.
+        document.getElementById(`temp${i+1}`).textContent = fiveDayForecast[i].main.temp + "&#176";
+        
+        //Set icon.
+        var iconULR = "http://openweathermap.org/img/wn/" + fiveDayForecast[i].main.icon + "@2x.png"
+        document.getElementById(`icon${i+1}`).setAttribute("src", iconURL);
+        document.getElementById(`icon${i+1}`).setAttribute("alt", fiveDayForecast[i].main.description);
+    }
+
+  });
---
+++ lesson11/js/weatherapiFH.js
@@ -0,0 +1,20 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5585010&units=imperial&appid=04076ec099a473169d26a85db37740a3';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    document.getElementByID('place').innerHTML=jsObject.name;
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('description').textContent = jsObject.weather.description;
+    document.getElementById('temp_max').textContent = jsObject.main.temp_max;
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('speed').textContent = jsObject.main.speed;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
